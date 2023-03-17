51641484d697e8f60b783433416c383f8adc8df0
Tim Beck <tmbeck@gmail.com>
2023-03-16 21:40:50 -0700

    Add chatgptplus

--- chatgpt.py
+++ chatgpt.py
@@ -3,17 +3,24 @@ import argparse
 
 # Set up argument parser
 parser = argparse.ArgumentParser(description='Chat with ChatGPT')
-parser.add_argument('--model', default='text-davinci-002', choices=['text-davinci-002', 'text-curie-001', 'text-babbage-001', 'text-ada-001'], help='OpenAI GPT-3 model to use')
+parser.add_argument('--model', default='text-davinci-002', choices=['text-davinci-002', 'text-curie-001', 'text-babbage-001', 'text-ada-001', 'text-davinci-002-001'], help='OpenAI GPT-3 model to use')
 parser.add_argument('--key', required=True, help='OpenAI API key')
 parser.add_argument('--temperature', default=0.7, type=float, help='Sampling temperature')
 parser.add_argument('--max_tokens', default=100, type=int, help='Maximum number of tokens to generate')
 parser.add_argument('--prompt', default='', help='Initial prompt for the conversation')
+parser.add_argument('--chatgptplus', action='store_true', help='Use a ChatGPT+ model')
 args = parser.parse_args()
 
 # Set up OpenAI API credentials
 openai.api_key = args.key
 
 # Set up ChatGPT
+if args.chatgptplus:
+    models = ['text-davinci-002-001', 'text-davinci-002']
+else:
+    models = ['text-davinci-002', 'text-curie-001', 'text-babbage-001', 'text-ada-001']
+if args.model not in models:
+    raise ValueError(f'Invalid model "{args.model}", must be one of {models}')
 def chat(prompt):
     response = openai.Completion.create(
         engine=args.model,
